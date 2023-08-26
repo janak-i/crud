dfb5648d33703763b56042555c1febb0677110cc
chirag garg <chirag@myrm.io>
2023-08-27 00:26:41 +0530

    some text in this

--- model/product.js
+++ model/product.js
@@ -8,3 +8,11 @@ Here are two common scenarios and the corresponding
 
 add dropdown in branch
 add button in master
+
+janak bhai how are you i hope you are fine?
+
+
+It will not update the working directory as git hard mode does.
+It will only reset the index but not the working tree, 
+then it generates the report of the files which have not been updated.
+

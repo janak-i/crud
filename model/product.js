a0ca9ecdfdd6d5c777a82fff8e37436ca797f8c7
chirag garg <chirag@myrm.io>
2023-08-27 00:01:59 +0530

    add drop down

--- model/product.js
+++ model/product.js
@@ -4,3 +4,6 @@ mohit tiwari how are you
 To reset a commit in a version control
 system like Git, you have a few options depending on your need
 Here are two common scenarios and the corresponding
+
+
+add dropdown in branch

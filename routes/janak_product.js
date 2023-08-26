589c2aa7b2143fc183f37b29eab6ad747175c565
chirag garg <chirag@myrm.io>
2023-08-26 23:36:59 +0530

    resolve CONFLICT

--- routes/janak_product.js
+++ routes/janak_product.js
@@ -16,5 +16,6 @@ janak how are you
 
 
 //===================================================//
+// ===================================================//
 Rohit Sharma is a prominent Indian cricketer and a well-known name in international cricket. 
 As of my last knowledge update in September 2021, here's some information about him
